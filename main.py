3389a6cdf28ce3d77108bdb52b515c514c2e85c3
DESKTOP-6BJPAG8\hello <hwjung@zinnotech.com>
2021-03-10 16:04:39 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import glfw
+
+# initializing glfw library
+if not glfw.init():
+    raise Exception("glfw can not be initialized!")
+
+# creating the window
+window = glfw.create_window(1280, 720, "My Opengl window", None, None)
+
+# check if window was created
+if not window:
+    glfw.terminate()
+    raise Exception("glfw window can not be created!")
+
+#set window`s position
+glfw.set_window_pos(window, 400, 200)
+
+# make the context current
+glfw.make_context_current(window)
+
+# main application loop
+while not glfw.window_should_close(window):
+    glfw.poll_events()
+
+    glfw.swap_buffers(window)
+
+# terminate glfw, free up allocated resources
+glfw.terminate()
